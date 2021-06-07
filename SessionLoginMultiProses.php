005cc0dc2c4c950c2dfa3d7f4e007d2dd2219c1f
M. Afada Nur Saiva Syahira <afadanursaiva2001@gmail.com>
2021-06-08 06:47:43 +0700

    Merubah menjadi SESSIONPROSES

--- SessionLoginMultiProses.php
+++ SessionLoginMultiProses.php
@@ -7,18 +7,18 @@
 
     $query="SELECT * FROM anggota WHERE noInduk='$username' and password='$password';";
     $result=mysqli_query($connect,$query);//dapat digunakan untuk melakukan kueri terhadap database.
-    $row=mysqli_fetch_assoc($result);//digunakan untuk mengambil baris hasil sebagai array asosiatif. Catatan: Nama kolom yang dikembalikan dari fungsi ini peka huruf besar / kecil(Case-sensitive).
+    $cek=mysqli_num_rows($result);
 
-    if($row['level']==1){
-        echo "Anda berhasil login,silahkan menuju "; ?>
-        <a href="">Halaman HOME</a>
-        <?php
-    }else if($row['level']==2){
-        echo "Anda berhasil login,silahkan menuju "; ?>
+    if($cek>0){
+        session_start();
+        $_SESSION['username']=$username;
+        $_SESSION['status']='login';?>
+        Login Berhasil
         <a href="HalamanUtama.html">Halaman HOME</a>
         <?php
     }else{
-        echo "Anda gagal login, silahkan ";?>
+        ?>
+        Anda gagal login, silahkan
         <a href="LoginForm.html">Login Kembali</a>
     <?php
         echo mysqli_error($connect);
