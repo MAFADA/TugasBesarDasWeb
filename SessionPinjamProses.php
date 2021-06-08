ed35f56a6e963c5e7a67fbdc085970e837808e77
M. Afada Nur Saiva Syahira <afadanursaiva2001@gmail.com>
2021-06-08 09:10:42 +0700

    prosess pinjam selesai(temp)

--- SessionPinjamProses.php
+++ SessionPinjamProses.php
@@ -2,17 +2,25 @@
     include "koneksi.php";
     session_start();
     $query="SELECT * FROM buku;";
+    $query1="SELECT * FROM peminjaman;";
+    $result1=mysqli_query($connect,$query);
     $result=mysqli_query($connect,$query);
+    $row1=mysqli_fetch_array($result1);
     $row=mysqli_fetch_array($result);
 
     $judul=$_GET['judul'];
     $jmlh=$_GET['jml'];
+    $datePinjam=$_GET['datepinjam'];
+    $dateKembali=$_GET['datekembali'];
 
     // pake loop buat cari data yg cocok untuk 
     // input ke database transaksi dan detail transaksi
 
         if($row['judul']==$judul){
-            $sql="INSERT INTO detail_pinjam(idPeminjaman,idBuku,jml) 
+            $peminjaman="INSERT INTO peminjaman(idAnggota,tglPinjam,tglKembali)
+            VALUES('$row1['idAnggota']','$datePinjam','$dateKembali');";
+            
+            $detail_pinjam="INSERT INTO detail_pinjam(idPeminjaman,idBuku,jml) 
             VALUES('$id_pinjam','$row['idBuku']',$jmlh);";
         }else{
             $_SESSION['error']='Data Buku yang anda masukkan tidak ditemukan';
