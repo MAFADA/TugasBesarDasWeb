30037ce3a6482cae40aa6f0fc3cefef0f82da3d1
M. Afada Nur Saiva Syahira <afadanursaiva2001@gmail.com>
2021-06-08 09:03:07 +0700

    edit isi

--- SessionPinjamProses.php
+++ SessionPinjamProses.php
@@ -8,10 +8,14 @@
     $judul=$_GET['judul'];
     $jmlh=$_GET['jml'];
 
-    if($row['judul']==$judul){
-        $sql="INSERT INTO ;";
-    }else{
-        $_SESSION['error']='Data Buku yang anda masukkan tidak ditemukan';
-        echo mysqli_error($connect);
-    }
+    // pake loop buat cari data yg cocok untuk 
+    // input ke database transaksi dan detail transaksi
+
+        if($row['judul']==$judul){
+            $sql="INSERT INTO detail_pinjam(idPeminjaman,idBuku,jml) 
+            VALUES('$id_pinjam','$row['idBuku']',$jmlh);";
+        }else{
+            $_SESSION['error']='Data Buku yang anda masukkan tidak ditemukan';
+            echo mysqli_error($connect);
+        }
 ?>
