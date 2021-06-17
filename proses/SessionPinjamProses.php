d0541f744777efd1a6d2dd730b9468fbc2df3ca3
M. Afada Nur Saiva Syahira <afadanursaiva2001@gmail.com>
2021-06-17 18:11:53 +0700

    proses pinjam halaman anggota berhasil

--- proses/SessionPinjamProses.php
+++ proses/SessionPinjamProses.php
@@ -9,16 +9,20 @@
     $jumlah=$_GET['jumlah'];
 
     $insert=mysqli_query($connect,"INSERT INTO peminjaman VALUES('','$idUser','$pinjam','$kembali','');");
-    if ($insert) {
-        
-        $masuk=mysqli_query($connect,"SELECT idPeminjaman FROM peminjaman ORDER BY DESC LIMIT 1");
+    if ($insert) {        
+        $masuk=mysqli_query($connect,"SELECT idPeminjaman FROM peminjaman ORDER BY idPeminjaman DESC LIMIT 1;");
+        $row=mysqli_fetch_array($masuk);
+        $id_pinjam=$row['idPeminjaman'];
         if ($masuk) {
-            $input=mysqli_query($connect,"INSERT INTO detail_pinjam VALUES('',SELECT idPeminjaman FROM peminjaman ORDER BY DESC LIMIT 1,'$idBuku',$jumlah,'');");    
+            $input=mysqli_query($connect,"INSERT INTO detail_pinjam VALUES('',$id_pinjam,'$idBuku',$jumlah,'');");    
             if ($input) {
-                echo "<script> alert('Data berhasil ditambah');</script>";
+                ?>
+                <script> alert('Data berhasil ditambah');</script>
+<?php
                 header('Location: ../halaman/Peminjaman.php');
             }else {
                 echo "Gagal".mysqli_error($connect);            
+                // header('Location: ../halaman/Peminjaman.php');
             }
         }
         
